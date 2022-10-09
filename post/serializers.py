a8be78117a3fdc66bc06b2cd303f5f3fef3b8a23
ohminyoung <minyoung_stat@dgu.ac.kr>
2022-10-10 01:58:18 +0900

    Post/Image Model,View,Serializer added

---
+++ post/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Post, Image
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['image']
+
+class PostSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'user', 'title', 'body', 'images')
+
+    def create(self, validated_data):
+        images_data = self.context['request'].FILES
+        post = Post.objects.create(**validated_data)
+        for image in images_data.getlist('images'):
+            Image.objects.create(post=post, image=image)
+
+
--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import PostList, PostSerializer
 
 app_name = "post"
 urlpatterns = [
-    
-]
+    path('posts/', PostList.as_view()),
+    path('posts/<int:post_id>', PostDetail.as_view()),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
