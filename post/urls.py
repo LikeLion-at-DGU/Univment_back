cad0c33fa77dc3ccf0c0d28fba500ae22cf96d3f
ohminyoung <minyoung_stat@dgu.ac.kr>
2022-10-10 21:04:22 +0900

    image uploading test

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views import *
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import PostList, PostSerializer
 
 app_name = "post"
 urlpatterns = [
     path('', PostList.as_view()),
     path('<int:pk>/', PostDetail.as_view()),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- univment/settings.py
+++ univment/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,7 +129,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
