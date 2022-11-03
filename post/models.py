8247725f067a320c78fc6eef052c5cfc49057040
ohminyoung <minyoung_stat@dgu.ac.kr>
2022-11-03 21:41:42 +0900

    post model added answers, event date, category field / added category model

--- post/models.py
+++ post/models.py
@@ -5,10 +5,15 @@ from accounts.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+    id = models.AutoField(primary_key = True)
+    generated_user = models.ForeignKey(User, null = False, on_delete = models.CASCADE)
+
 class Post(models.Model):
     id = models.AutoField(primary_key = True)
     user = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
     title = models.CharField(max_length = 200)
-    body = models.TextField()
+    answers = models.JSONField()
     image = models.ImageField()
-
+    event_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name = 'posts')
--- post/serializers.py
+++ post/serializers.py
@@ -2,16 +2,10 @@ from rest_framework import serializers
 from .models import Like, Post
 
 
-class LikeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Like
-        fields = ['user', 'post', 'created']
-
 class PostSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Post
-        fields = ('id', 'user', 'title', 'body', 'likes', 'image')
+        fields = ('id', 'user', 'title', 'answers', 'image', 'event_date', 'category')
 
     def create(self, validated_data):
         post = Post.objects.create(**validated_data)
