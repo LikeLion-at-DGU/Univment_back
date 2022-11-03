3c7ac686744d78edddbfe56c0425bfdab68c223f
ohminyoung <minyoung_stat@dgu.ac.kr>
2022-11-03 20:08:11 +0900

    images to image

--- post/models.py
+++ post/models.py
@@ -10,15 +10,5 @@ class Post(models.Model):
     user = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
     title = models.CharField(max_length = 200)
     body = models.TextField()
+    image = models.ImageField()
 
-    def get_total_likes(self):
-        return self.likes.user.count()
-
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name="likes")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name = "likes")
-    created = models.DateTimeField(auto_now_add = True)
-
-class Image(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None, related_name = 'images')
-    image = models.ImageField(upload_to = 'images/')
--- post/serializers.py
+++ post/serializers.py
@@ -1,10 +1,6 @@
 from rest_framework import serializers
-from .models import Like, Post, Image
+from .models import Like, Post
 
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Image
-        fields = ['image']
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,19 +8,13 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = ['user', 'post', 'created']
 
 class PostSerializer(serializers.ModelSerializer):
-    images = ImageSerializer(many=True, read_only=True, default=None)
-    likes = LikeSerializer(many=True, default=None)
 
     class Meta:
         model = Post
-        fields = ('id', 'user', 'title', 'body', 'likes', 'images')
+        fields = ('id', 'user', 'title', 'body', 'likes', 'image')
 
     def create(self, validated_data):
-        images_data = self.context['request'].FILES
         post = Post.objects.create(**validated_data)
-        for image in images_data.getlist('images'):
-            Image.objects.create(post=post, image=image)
-        
         return post
 
 
