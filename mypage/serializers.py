60f041e2253ef6e4db58f964c1d12a6fff9c7e9a
dudtlstm <annpys1024@naver.com>
2022-11-08 04:38:06 +0900

    namecard club,contests,projects,activities 추가

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -4,19 +4,40 @@ from .models import *
 class ProfileImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfileImage
-        fields = ('id', 'profileimage')
+        fields = ('id', 'user', 'profileimage')
 
     def create(self, validated_data):
         profileimage = ProfileImage.objects.create(**validated_data)
         return profileimage
 
+#NameCard(명함) 관련된 시리얼라이저
 class NameCardProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('id', 'birthday', 'major')
+        fields = ('id', 'user', 'birthday', 'major')
 
 class NameCardContactsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contacts
-        fields = ('id', 'phonenumber', 'email', 'insta', 'github', 'blog')
+        fields = ('id', 'user', 'phonenumber', 'email', 'insta', 'github', 'blog')
+
+class NameCardClubsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clubs
+        fields = ('id', 'user', 'club1', 'club2', 'club3', 'club4', 'club5')
+
+class NameCardContestsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contests
+        fields = ('id', 'user', 'contest1', 'contest2', 'contest3', 'contest4', 'contest5')
+
+class NameCardProjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ('id', 'user', 'project1', 'project2', 'project3', 'project4', 'project5')
+
+class NameCardActivitiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activities
+        fields = ('id', 'user', 'activity1', 'activity2', 'activity3', 'activity4', 'activity5')
         
