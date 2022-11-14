394d350281e6de52eb52c2a6de83dcf3e870c374
ohminyoung <minyoung_stat@dgu.ac.kr>
2022-11-14 19:18:23 +0900

    color added

--- post/models.py
+++ post/models.py
@@ -7,19 +7,20 @@ from accounts.models import User
 
 class Category(models.Model):
     id = models.AutoField(primary_key = True)
-    name = models.CharField(null = True, max_length = 100)
-    isDefault = models.BooleanField(default = False)
-    generated_user = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 100)
+    color = models.CharField(max_length=20, null=True)
+    isDefault = models.BooleanField(null = False, default=False)
+    generated_user = models.ForeignKey(User, null = False, on_delete = models.CASCADE)
     questions = models.JSONField(null = True)
     class Meta:
         unique_together = ["name", "generated_user"]
 
 class Post(models.Model):
     id = models.AutoField(primary_key = True)
-    user = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
-    title = models.CharField(max_length = 200, default = '')
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 1000, null=False, default = '')
     answers = models.JSONField(null = True)
     image = models.ImageField(null = True)
-    event_date = models.DateField(default=datetime.date.today)
+    event_date = models.DateField(default=datetime.date.today, null=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name = 'posts', null = True)
     timeline = models.BooleanField(default=False)
